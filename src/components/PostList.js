5088e2b501dea9017cb2fda23b723ad5dafac348
Apzure <block4556@gmail.com>
2023-01-04 02:57:04 +0800

    PostList TopBar

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,8 +4,8 @@ function PostList({posts}){
     const renderedPosts = posts.map(post => {
         const {title, creator, time} = post;
 
-        return ( //will have to change link to remove spaces and possible duplicates, key as well 
-            <Link to={title} key={title}> 
+        return ( //will have to change link to remove spaces and possible duplicates, for key as well (add key)
+            <Link to={title}> 
                 <div className="flex place-content-between px-4 py-2 hover:bg-slate-100">
                     <div>
                         <div className='text-green-600'> 
@@ -16,7 +16,12 @@ function PostList({posts}){
                         </div>
                     </div>
                     <div>
-                        Views, Replies
+                        <div>
+                            123 Views
+                        </div>
+                        <div>
+                            4 Replies
+                        </div>
                     </div>
                 </div>
                 <hr></hr>
--- src/components/PostTopbar.js
+++ src/components/PostTopbar.js
@@ -1,17 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function PostTopbar({topic}) {
+    const [IsDropdownOpen, setDropdownOpen] = useState(false);
+    const Dropdown = 
+        <div className="absolute bg-amber-300  ">
+            <div>
+                Recent
+            </div>
+            <div>
+                Hot
+            </div>
+            <div>
+                Top
+            </div>
+        </div>
+    const handleDropdown = () => {
+        setDropdownOpen(!IsDropdownOpen);
+    };
     return (
-        <div className="flex place-content-between pb-2 mb-5 border-b-4 border-lime-800" >
-            <div className="ml-4 w-[800px]">
+        <div className="flex place-content-between pb-2 mb-5 border-b-4 border-lime-800 " >
+            <div className="ml-4 w-[700px]">
                 <div className="text-2xl">
                     {topic.name}
                 </div>
-                <div className="text-stone-600 text-sb">
+                <div className="text-stone-600 text-sm">
                     {topic.desc}
                 </div>
             </div>
-            <Link to='CreatePost' className="mr-4 self-center bg-green-400 rounded-sm p-2 hover:bg-green-500">
+            <div className="self-center bg-amber-300  rounded-md mt-4" onClick={handleDropdown}>
+                <div className="p-1">
+                    Sort by Recent
+                </div>
+                {IsDropdownOpen && Dropdown}
+            </div>
+            <Link to='CreatePost' className="mr-4 self-center bg-green-400 rounded-lg p-2 mt-4 hover:bg-green-500">
                 Create New Post
             </Link>
         </div>
